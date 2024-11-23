638f8d710a681cb6fdc41ce4253683be8e74198f
Vix <Gordan.ei@protonmail.ch>
2024-11-24 00:40:37 +0400

    nav link component created

---
+++ frontend/components/dashboard/nav-link.vue
@@ -0,0 +1,37 @@
+<template>
+    <nuxt-link
+        :to="
+            to === 'dashboard' || to === 'logout'
+                ? '/' + to
+                : '/dashboard/' + to
+        "
+        class="group flex items-center justify-between gap-2 rounded-md border border-transparent px-2.5 py-2 text-sm text-slate-900 font-semibold active:border-indigo-200"
+    >
+        <slot>
+            <!-- Icon slot -->
+        </slot>
+        <span class="grow">
+            {{ label }}
+        </span>
+        <span
+            v-if="helper"
+            class="inline-flex items-center justify-center px-1 text-xs text-indigo-900 border border-indigo-200 rounded-full bg-indigo-50"
+        >
+            {{ helper }}
+        </span>
+    </nuxt-link>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+    to: string;
+    label: string;
+    helper?: string;
+}>();
+</script>
+
+<style scoped>
+.router-link-exact-active {
+    @apply bg-indigo-100 text-indigo-500 hover:bg-indigo-100 hover:text-indigo-600;
+}
+</style>
