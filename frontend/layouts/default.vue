b620cf922f651f8e558a9cc07f1d24d5476882dd
Anton Vix <Gordan.ei@protonmail.ch>
2024-11-25 05:32:48 +0400

    dynamic header and footer added

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,9 +1,23 @@
 <template>
     <div class="page-transition layout-transition">
-        <Header-1 />
+        <component :is="headerComponent" />
 
         <slot></slot>
 
-        <Footer-1 />
+        <component :is="footerComponent" />
     </div>
 </template>
+
+<script lang="ts" setup>
+import { defineAsyncComponent } from 'vue';
+const header: string = 'default';
+const footer: string = 'default';
+
+const headerComponent = computed(() =>
+    defineAsyncComponent(() => import(`@/components/header/${header}.vue`))
+);
+
+const footerComponent = computed(() =>
+    defineAsyncComponent(() => import(`@/components/footer/${footer}.vue`))
+);
+</script>
