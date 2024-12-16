b553e3ccf555db4fc84a87040e1aabd396760881
Vix <Gordan.ei@protonmail.ch>
2024-12-16 22:28:05 +0400

    statistics update

--- backend/app/Http/Controllers/StatisticsController.php
+++ backend/app/Http/Controllers/StatisticsController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Helpers\ApiResponse;
+use App\Models\Order;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 
@@ -20,16 +21,16 @@ class StatisticsController extends Controller
                 'this_month' => User::where('created_at', '>=', now()->subMonth())->count(),
             ],
             'sales' => [
-                'total' => 0,
-                'today' => 0,
-                'this_week' => 0,
-                'this_month' => 0,
+                'total' => Order::count(),
+                'today' => Order::where('created_at', '>=', now()->subDay())->count(),
+                'this_week' => Order::where('created_at', '>=', now()->subWeek())->count(),
+                'this_month' => Order::where('created_at', '>=', now()->subMonth())->count(),
             ],
             'earnings' => [
-                'total' => 0,
-                'today' => 0,
-                'this_week' => 0,
-                'this_month' => 0,
+                'total' => Order::sum('total'),
+                'today' => Order::where('created_at', '>=', now()->subDay())->sum('total'),
+                'this_week' => Order::where('created_at', '>=', now()->subWeek())->sum('total'),
+                'this_month' => Order::where('created_at', '>=', now()->subMonth())->sum('total'),
             ],
         ]);
     }
