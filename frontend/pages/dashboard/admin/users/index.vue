68e3bc20b732db653d334a7ae159ac13e6cf5adf
Vix <Gordan.ei@protonmail.ch>
2025-01-10 19:23:55 +0400

    users - pagination added

--- frontend/pages/dashboard/admin/users/index.vue
+++ frontend/pages/dashboard/admin/users/index.vue
@@ -18,10 +18,22 @@
             }"
             @select="select"
         />
+
+        <div
+            class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700"
+        >
+            <u-pagination
+                v-model="page"
+                :active-button="{ variant: 'outline', color: 'blue' }"
+                :inactive-button="{ color: 'gray' }"
+                :total="total"
+            />
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
+import type { Meta } from '~/types/pagination';
 import type { User } from '~/types/user';
 
 definePageMeta({
@@ -62,16 +74,36 @@ const columns = [
 
 const users = ref<User[]>([]);
 const status = ref('pending');
-onMounted(async () => {
-    const response: { data: User[] } = await apiFetch<{ data: User[] }>(
-        `v1/users`
-    );
 
-    users.value = response.data;
-    status.value = 'success';
+const page = ref(1);
+const total = ref(0);
+
+onMounted(async () => {
+    await getUsers();
+});
+
+watch(page, async () => {
+    await getUsers();
 });
 
 function select(row: User) {
     return navigateTo('/dashboard/admin/users/' + row.id);
+}
+
+async function getUsers() {
+    const { data, meta } = await apiFetch<{ data: User[]; meta: Meta }>(
+        `v1/users`,
+        {
+            params: {
+                page: page.value,
+            },
+        }
+    );
+
+    users.value = data;
+    status.value = 'success';
+
+    page.value = meta.current_page;
+    total.value = meta.total;
 }
 </script>
