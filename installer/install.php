c38d9d6fc7780752452586c9e4383a01ea2edc26
Anton Vix <Gordan.ei@protonmail.ch>
2025-01-14 05:19:42 +0400

    Installer renamed

--- installer/install.php
+++ installer/install.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/src/php/installer.php';
+require_once __DIR__ . '/src/php/Installer.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$installer = new Installer($_POST);
