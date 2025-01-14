2095bc0c1d133b8169f424f13955c11c62e89c18
Anton Vix <Gordan.ei@protonmail.ch>
2025-01-14 17:06:35 +0400

    env helper refactor

--- installer/src/php/Env.php
+++ installer/src/php/Env.php
@@ -18,29 +18,36 @@ class Env
 
 	public function get(string $key): string
 	{
-		$envContent = file_get_contents($this->envFile);
-
-		return explode("{$key}=", $envContent)[1];
+		return explode("{$key}=", $this->getEnvContent())[1];
 	}
 
 	public function set(string $key, string $value): void
 	{
-		$envContent = file_get_contents($this->envFile);
-		$envContent = str_replace("{$key}=", "{$key}={$value}", $envContent);
-		file_put_contents($this->envFile, $envContent);
+		$this->writeToEnvFile(
+			str_replace("{$key}=", "{$key}={$value}", $this->getEnvContent())
+		);
 	}
 
 	public function delete(string $key): void
 	{
-		$envContent = file_get_contents($this->envFile);
-		$envContent = str_replace("{$key}=", "", $envContent);
-		file_put_contents($this->envFile, $envContent);
+		$this->writeToEnvFile(
+			str_replace("{$key}=", "", $this->getEnvContent())
+		);
 	}
 
 	public function exists(string $key): bool
 	{
-		$envContent = file_get_contents($this->envFile);
-		return strpos($envContent, "{$key}=") !== false;
+		return strpos($this->getEnvContent(), "{$key}=") !== false;
+	}
+
+	protected function getEnvContent(): string
+	{
+		return file_get_contents($this->envFile);
+	}
+
+	protected function writeToEnvFile(string $content): void
+	{
+		file_put_contents($this->envFile, $content);
 	}
 
 	public function createFromExample(): void
