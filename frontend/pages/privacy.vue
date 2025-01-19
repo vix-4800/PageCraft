aa86f42e7459327f0bdce7aae572bc23f5404866
Vix <Gordan.ei@protonmail.ch>
2025-01-19 18:58:23 +0400

    empty terms and privacy page

---
+++ frontend/pages/privacy.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold text-center">Privacy Policy</h1>
+    </div>
+</template>
+
+<script lang="ts" setup></script>
---
+++ frontend/pages/terms.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold text-center">Terms of Service</h1>
+    </div>
+</template>
+
+<script lang="ts" setup></script>
