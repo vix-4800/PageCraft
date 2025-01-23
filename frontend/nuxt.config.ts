1df10323cd9c2ad69edaf35ac8dcf7c76839c1ce
Vix <Gordan.ei@protonmail.ch>
2025-01-24 01:30:58 +0400

    seo module installed

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -16,6 +16,7 @@ export default defineNuxtConfig({
         '@nuxt/ui',
         'nuxt-echarts',
         'pinia-plugin-persistedstate/nuxt',
+        '@nuxtjs/seo',
     ],
     plugins: [{ src: '~/plugins/notify', mode: 'client' }],
     app: {
