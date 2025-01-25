76ce6543e858f60ba3d30069b19cb277f6ec08bd
Vix <Gordan.ei@protonmail.ch>
2025-01-25 23:12:49 +0400

    basic DatabaseBackupTest created

---
+++ backend/tests/Feature/DatabaseBackupTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class DatabaseBackupTest extends TestCase
+{
+    public function test_backup_create_command(): void
+    {
+        //
+    }
+
+    public function test_backup_clear_command(): void
+    {
+        //
+    }
+}
