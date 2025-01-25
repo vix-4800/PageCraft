9b71192afa1455a23547e180131a1017a6e4239a
Vix <Gordan.ei@protonmail.ch>
2025-01-25 04:52:18 +0400

    AccountPasswordReset notification added

---
+++ backend/resources/views/mail/account-password-reset.blade.php
@@ -0,0 +1,10 @@
+<x-mail::message>
+    # Password Reset
+
+    Your password has been reset.
+
+    If you did not request a password reset, please contact us immediately.
+
+    Regards,
+    {{ config('app.name') }}
+</x-mail::message>
