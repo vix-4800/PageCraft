be824917e8c5812c2c8b0be0a38dc2a9d8a76dba
Vix <Gordan.ei@protonmail.ch>
2025-01-26 00:52:50 +0400

    Reverb installed

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
+        channels: __DIR__.'/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
