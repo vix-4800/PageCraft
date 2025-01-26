7cbd234a49f5a0d51b750b1f086db8a2998da352
Vix <Gordan.ei@protonmail.ch>
2025-01-27 03:22:06 +0400

    scheduled backups created

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -1,3 +1,9 @@
 <?php
 
 declare(strict_types=1);
+
+use App\Console\Commands\CreateDatabaseBackup;
+use Illuminate\Console\Scheduling\Schedule as ScheduleContract;
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::call(CreateDatabaseBackup::class)->dailyAt('01:00')->days([ScheduleContract::SUNDAY, ScheduleContract::WEDNESDAY]);
