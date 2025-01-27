8e7bd07188d26b229f5a4ddf699f254c355335bf
Vix <Gordan.ei@protonmail.ch>
2025-01-27 23:44:23 +0400

    about component divided

---
+++ frontend/components/metrics/basic.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="grid gap-6 lg:grid-cols-4 sm:grid-cols-2 max-lg:gap-12">
+        <div v-for="stat in stats" :key="stat.label" class="text-center">
+            <h3 class="text-4xl font-extrabold text-gray-800">
+                {{ stat.value
+                }}<span class="text-blue-600">{{ stat.suffix }}</span>
+            </h3>
+            <p class="mt-4 text-base font-bold">{{ stat.label }}</p>
+            <p class="mt-2 text-sm text-gray-500">
+                {{ stat.description }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+    stats: {
+        type: Array,
+        required: true,
+    },
+});
+</script>
---
+++ frontend/components/team/basic.vue
@@ -0,0 +1,57 @@
+<template>
+    <div
+        class="grid gap-6 mt-12 lg:grid-cols-4 md:grid-cols-3 max-md:justify-center"
+    >
+        <div
+            v-for="member in teamMembers"
+            :key="member.id"
+            class="overflow-hidden border rounded-lg"
+        >
+            <nuxt-img :src="member.image" class="object-cover w-full h-56" />
+
+            <div class="p-4">
+                <h4 class="text-base font-bold text-gray-800">
+                    {{ member.name }}
+                </h4>
+                <p class="mt-1 text-xs text-gray-800">
+                    {{ member.role }}
+                </p>
+
+                <div class="mt-4 space-x-4">
+                    <u-button
+                        icon="mdi:facebook"
+                        color="blue"
+                        :to="member.facebook"
+                        target="_blank"
+                        :ui="{ rounded: 'rounded-full' }"
+                    />
+
+                    <u-button
+                        icon="mdi:twitter"
+                        color="blue"
+                        :to="member.twitter"
+                        target="_blank"
+                        :ui="{ rounded: 'rounded-full' }"
+                    />
+
+                    <u-button
+                        icon="mdi:instagram"
+                        color="blue"
+                        :to="member.instagram"
+                        target="_blank"
+                        :ui="{ rounded: 'rounded-full' }"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+    teamMembers: {
+        type: Array,
+        required: true,
+    },
+});
+</script>
