dfa0cc065220b33e756acfc29254d5d3bb380948
Anton Vix <Gordan.ei@protonmail.ch>
2025-01-30 00:41:43 +0400

    article default component update

--- frontend/components/article-detail/default.vue
+++ frontend/components/article-detail/default.vue
@@ -1,5 +1,26 @@
 <template>
-    <div v-if="article"></div>
+    <div v-if="article">
+        <div class="pt-16 text-center">
+            <p class="text-sm font-bold text-green-500 md:text-base">
+                {{ article.created_at }} <span class="text-gray-900">/</span>
+                {{ article.author.toUpperCase() }}
+            </p>
+            <h1 class="text-3xl font-bold break-normal md:text-5xl">
+                {{ article.title }}
+            </h1>
+        </div>
+
+        <nuxt-img
+            :src="article.image"
+            :alt="article.title"
+            class="container w-full mt-8 bg-white bg-cover rounded"
+        />
+
+        <div
+            v-html="article.content"
+            class="w-full max-w-full p-8 px-16 -mt-32 text-xl leading-normal prose text-gray-800 bg-white md:text-2xl"
+        ></div>
+    </div>
 </template>
 
 <script lang="ts" setup>
