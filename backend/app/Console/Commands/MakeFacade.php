2bbc1057faff5d572cd2d4f7e982d9cf99eb18a9
Vix <Gordan.ei@protonmail.ch>
2025-02-06 19:29:10 +0400

    make facade command - promptForMissingArgumentsUsing

--- backend/app/Console/Commands/MakeFacade.php
+++ backend/app/Console/Commands/MakeFacade.php
@@ -47,4 +47,50 @@ class MakeFacade extends Command implements PromptsForMissingInput
 
         $this->info("Facade {$name} created!");
     }
+
+    /**
+     * Prompt for missing input arguments using the returned questions.
+     *
+     * @return array<string, string>
+     */
+    protected function promptForMissingArgumentsUsing(): array
+    {
+        return [
+            'name' => fn (): int|string => search(
+                label: 'What is the name of the facade?',
+                placeholder: 'E.g. MyLogger',
+                options: function (string $value): array {
+                    if (strlen($value) === 0) {
+                        return [];
+                    }
+
+                    // Get all services
+                    $services = $this->recursiveScanDir();
+
+                    // Remove file extensions
+                    $services = array_map(fn (string $service): string => str_replace('.php', '', $service), $services);
+
+                    return array_filter(
+                        $services,
+                        fn (string $service): bool => str_contains(strtolower($service), strtolower($value))
+                    );
+                }
+            ),
+        ];
+    }
+
+    private function recursiveScanDir(string $dir = ''): array
+    {
+        $path = app_path("Services/{$dir}");
+        $allFiles = scandir($path);
+
+        $services = array_filter($allFiles, fn (string $file): bool => ! is_dir("{$path}/{$file}") && str_ends_with($file, '.php'));
+
+        $folders = array_filter($allFiles, fn (string $file): bool => is_dir("{$path}/{$file}") && ! in_array($file, ['.', '..']));
+        foreach ($folders as $folder) {
+            $services = array_merge($services, $this->recursiveScanDir("{$dir}/{$folder}"));
+        }
+
+        return $services;
+    }
 }
