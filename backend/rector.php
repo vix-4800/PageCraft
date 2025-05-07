899c512c6f3d5b3f9f215abec78d040d004d9656
Anton Vix <Gordan.ei@protonmail.ch>
2025-05-07 23:44:58 +0400

    rector installed

---
+++ backend/rector.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelLevelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/bootstrap',
+        __DIR__.'/config',
+        __DIR__.'/public',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
+    ])
+    // uncomment to reach your current PHP version
+    ->withPhpSets()
+    ->withSets([
+        LaravelLevelSetList::UP_TO_LARAVEL_110,
+    ])
+    ->withPreparedSets(
+        codingStyle: true,
+        naming: true,
+        privatization: true,
+        rectorPreset: true,
+    )
+    ->withImportNames(removeUnusedImports: true)
+    ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(0);
